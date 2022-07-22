e92b1c70a7bf73dbd2ce7a3273f4caa4cccab273
victoria <victoria97molina@yahoo.com>
2022-07-21 23:52:39 -0500

    Initial Commit

---
+++ IstaFilterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IstaFilterApp
+//
+//  Created by Rolando Orta on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IstaFilterApp/IstaFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IstaFilterAppApp.swift
+//  IstaFilterApp
+//
+//  Created by Rolando Orta on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct IstaFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
